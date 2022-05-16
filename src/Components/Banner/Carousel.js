eb1606782ecb45399e19fcf8302619c0b3386018
franciscocruz29 <franciscohuertas25@gmail.com>
2022-05-16 18:44:03 -0500

    Completed Carousel component

--- src/Components/Banner/Carousel.js
+++ src/Components/Banner/Carousel.js
@@ -6,6 +6,10 @@ import { CryptoState } from "../../Pages/CryptoContext";
 import AliceCarousel from "react-alice-carousel";
 import { Link } from "react-router-dom";
 
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 const useStyles = makeStyles((theme) => ({
   carousel: {
     height: "50%",
@@ -25,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
 const Carousel = () => {
   const [trending, setTrending] = useState([]);
   const classes = useStyles();
-  const { currency } = CryptoState();
+  const { currency, symbol } = CryptoState();
 
   const fetchTrendingCoins = async () => {
     const { data } = await axios.get(TrendingCoins(currency));
@@ -37,6 +41,7 @@ const Carousel = () => {
   }, [currency]);
 
   const items = trending.map((coin) => {
+    let profit = coin?.price_change_percentage_24h >= 0;
     return (
       <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
         <img
@@ -45,6 +50,22 @@ const Carousel = () => {
           height="80"
           style={{ marginBottom: 10 }}
         />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span
+            style={{
+              color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+              fontWeight: 500,
+            }}
+          >
+            {profit && "+"}
+            {coin?.price_change_percentage_24h?.toFixed(2)} %
+          </span>
+        </span>
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
       </Link>
     );
   });
@@ -66,11 +87,11 @@ const Carousel = () => {
         autoPlayInterval={1000}
         animationDuration={1500}
         disableDotsControls
+        disableButtonsControls
         responsive={responsive}
         autoPlay
         items={items}
       />
-
     </div>
   )
 }
